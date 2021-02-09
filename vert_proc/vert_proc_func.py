daf03d4567b78de7d14bf04b9a51af4777619818
Rich Neale <rneale@ucar.edu>
2021-02-09 15:55:35 -0700

    Add vertical proc directory and initial files

---
+++ vert_proc/vert_proc_func.py
@@ -0,0 +1,5 @@
+######################################################
+## PYTHON FUNCTIONS FOR VERTICAL PROCESSES NOTEBOOK ##
+######################################################
+
+print('+++ IMPORTING MY FUNCTIONS from vert_proc_func.py +++')
