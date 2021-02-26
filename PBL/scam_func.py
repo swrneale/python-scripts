d794dfc7f48fa8bb2959fe7e5726a5e6e6f405f5
Rich Neale <rneale@ucar.edu>
2021-02-26 08:55:07 -0700

    SCAM Perdigao and quick-look updates

--- PBL/scam_func.py
+++ PBL/scam_func.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as mp
 ### Constants ###
 
 r_gas = mconst.dry_air_gas_constant.magnitude   # Specific gas constant for dry air (kg/joule)
-cp_air = mconst.dry_air_spec_heat_press.magnitude # Specific heat for dry air
+cp_air = mconst.magnitude # Specific heat for dry air
 Lv = mconst.water_heat_vaporization.magnitude       # Latent heat of vaporization
 grav = mconst.earth_gravity.magnitude
 
@@ -872,7 +872,7 @@ def plot1d_snap_scam(rinfo):
                 
                          
                 if var not in ['T','TH','THL']: 
-                    mp.vlines(0, 0, scam_icase[pvar.dims[1]].max(), linestyle="dashed",lw=1)
+                   %matplotlib inline
                 mp.hlines(zlev, cmin, cmax,lw=0.01) # plev horizontal lines
                 
 ## PLOT PBLH DEPEDENT ON QUANTITY ###
