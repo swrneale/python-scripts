335da485384c38b71084b8f82f1dbee4e2bb5dbe
Rich Neale <rneale@ucar.edu>
2022-04-27 11:30:51 -0600

    General scripts update

---
+++ general/cam_proc_utils.py
@@ -0,0 +1,39 @@
+#########################################
+
+# CAM PROCESSING UTILS FUNCTIONS
+
+#########################################
+
+print('+++ IMPORTING UTILS FUNCTIONS +++')
+
+### Imports ###
+import numpy as np
+import xarray as xr
+from scipy.interpolate import interp1d
+import scipy as spy
+import pandas as pd
+import datetime as dt
+import cftime as cft
+from nc_time_axis import CalendarDateTime
+import subprocess as sp	
+
+import metpy.constants as mconst
+import metpy.calc as mpc
+	
+
+    
+def cam_dev_run_get(for_season):
+    
+    return cam_runs
+
+
+def cam_dev_runs_stats(cam_runs_in):
+
+
+
+    return run_mean,run_rmse,run_correlation
+
+
+def cam_dev_table(run_mean,run_rmse,run_correlation)
+
+    return 
